c3756c526b954d48a7995fb8a29ec0cf46595f3b
lawliet <coffeelancerr@gmail.com>
2025-04-11 16:01:26 +0300

    newnew

--- SportsStore/Controllers/OrderController.cs
+++ SportsStore/Controllers/OrderController.cs
@@ -3,8 +3,29 @@ using SportsStore.Models;
 
 namespace SportsStore.Controllers
 {
-    public class OrderController : Controller
+    public class OrderController(IOrderRepository repository, Cart cartService) : Controller
     {
+        private readonly IOrderRepository _orderRepository = repository;
+        private readonly Cart _cart = cartService;
+
         public ViewResult Checkout() => View(new Order());
+
+        [HttpPost]
+        public IActionResult Checkout(Order order)
+        {
+            if (_cart.Lines.Count == 0)
+                ModelState.AddModelError("", "Sorry, your cart is empty!");
+
+            if (ModelState.IsValid)
+            {
+                order.Lines = _cart.Lines.ToArray();
+                _orderRepository.SaveOrder(order);
+                _cart.Clear();
+                return RedirectToPage("/Completed",
+                    new { orderId = order.Id });
+            }
+            else
+                return View();
+        }
     }
 }
---
+++ SportsStore/Migrations/20250411113425_ShippedOrders.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SportsStore.Migrations
+{
+    /// <inheritdoc />
+    public partial class ShippedOrders : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsShipped",
+                table: "Orders",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsShipped",
+                table: "Orders");
+        }
+    }
+}
